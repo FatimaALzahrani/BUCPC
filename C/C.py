16819939e53041c30451a8dcb7d114400d5a60fd
Fatima Mohammed  Alzahrani <107775566+FatimaALzahrani@users.noreply.github.com>
2023-09-18 22:27:04 +0300

    .

--- C/C.py
+++ C/C.py
@@ -1,27 +1,17 @@
-T = int(input())
+s, k = input().split()
+k = int(k)
+re = '0'
 
-for _ in range(T):
-    n = int(input())
-    Ar = []
-    tmp = 0
-    key = 0
+for i in range(len(s)):
     count = 0
-    k = n
-    for j in range(n):
-        Ar.append(int(input()))
-    
-    while True:
-        for j in range(len(Ar)):
-            if Ar[j] < tmp:
-                tmp = Ar[j]
-                key = j
-        for j in range(key,len(Ar)):
-            Ar.pop(key)
-        count += 1
-        k = len(Ar)
-        if not Ar:
-            break
-    if count % 2 == 0:
-        print("Hoor")
-    else:
-        print("Majd")
+    for j in range(len(s)):
+        if s[i] == s[j]:
+            count += 1
+    if count == k:
+        re = s[i]
+        break
+
+if re == '0':
+    print("NO")
+else:
+    print("YES " + re)
